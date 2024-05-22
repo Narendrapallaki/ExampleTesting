43124b5692ecb9fdd2c36394899b17f636bd161d
Narendrapallaki <149230075+Narendrapallaki@users.noreply.github.com>
2024-05-22 02:07:37 -0700

    Delete LoanApplication directory
    Un known one

--- LoanApplication/src/main/java/com/loan/droolsConfig/DroolsConfig.java
+++
@@ -1,5 +0,0 @@
-package com.loan.droolsConfig;
-
-public class DroolsConfig {
-
-}
--- LoanApplication/src/main/java/com/loan/loanService/LoanService.java
+++
@@ -1,8 +0,0 @@
-package com.loan.loanService;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoanService {
-
-}
